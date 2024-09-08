109fff04ac355d51d44f3f2db414e175c7c29740
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2024-09-08 11:26:41 +0530

    Add files via upload
    added initial files

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+import pandas as pd
+import yfinance as yf
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e6f469c376e0724c6cc76a694549c290b02ff4ba56f727c0'
+
+messages = {}
+
+@app.route("/", methods=('GET', 'POST'))
+def main():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = request.form['quantity']
+
+        if not name:
+            flash('name is required!')
+        elif not quantity:
+            flash('Quantity is required!')
+        else:
+            messages[name] = quantity
+            return redirect(url_for('main'))
+
+    length = len(messages)
+
+    beta = 0
+
+    if len(messages) == 0:
+        beta = 0
+    
+    elif len(messages) == 1:
+        key = list(messages.keys())[0]
+        beta = yf.Ticker(key+".ns").info['beta']
+    
+    else:
+        total_weight = 0
+        for key, value in messages.items():
+            total_weight += yf.Ticker(key+".ns").info['currentPrice'] * int(value)
+        
+        for key,value in messages.items():
+            weight = yf.Ticker(key+".ns").info['currentPrice'] * int(value) / total_weight
+            beta += yf.Ticker(key+".ns").info['beta'] * weight
+    
+    return render_template('base.html', messages=messages, length = length,beta=beta)
+
