2de796f476e03e559342ddb312d53982fada51bc
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2024-09-23 19:26:57 +0530

    changed contact route to about route

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ import pandas as pd
 import yfinance as yf
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e6f469c376e0724c6cc76a694549c290b02ff4ba56f727c0'
+app.config["SECRET_KEY"] = "e6f469c376e0724c6cc76a694549c290b02ff4ba56f727c0"
 
 messages = {}
 stocks = list()
 
-@app.route("/old", methods=('GET', 'POST'))
+
+@app.route("/old", methods=("GET", "POST"))
 def main():
-    if request.method == 'POST':
-        name = request.form['name']
-        quantity = request.form['quantity']
+    if request.method == "POST":
+        name = request.form["name"]
+        quantity = request.form["quantity"]
 
         if not name:
-            flash('name is required!')
+            flash("name is required!")
         elif not quantity:
-            flash('Quantity is required!')
+            flash("Quantity is required!")
         else:
             messages[name] = quantity
-            return redirect(url_for('main'))
+            return redirect(url_for("main"))
 
     length = len(messages)
 
@@ -28,53 +29,56 @@ def main():
 
     if len(messages) == 0:
         beta = 0
-    
+
     elif len(messages) == 1:
         key = list(messages.keys())[0]
-        beta = yf.Ticker(key+".ns").info['beta']
-    
+        beta = yf.Ticker(key + ".ns").info["beta"]
+
     else:
         total_weight = 0
         for key, value in messages.items():
-            total_weight += yf.Ticker(key+".ns").info['currentPrice'] * int(value)
-        
-        for key,value in messages.items():
-            weight = yf.Ticker(key+".ns").info['currentPrice'] * int(value) / total_weight
-            beta += yf.Ticker(key+".ns").info['beta'] * weight
-    
-    return render_template('base.html', messages=messages, length = length,beta=beta)
+            total_weight += yf.Ticker(key + ".ns").info["currentPrice"] * int(value)
+
+        for key, value in messages.items():
+            weight = (
+                yf.Ticker(key + ".ns").info["currentPrice"] * int(value) / total_weight
+            )
+            beta += yf.Ticker(key + ".ns").info["beta"] * weight
+
+    return render_template("base.html", messages=messages, length=length, beta=beta)
 
 
-@app.route('/', methods=["GET"])
-@app.route('/home', methods = ["GET", "POST"])
+@app.route("/", methods=["GET"])
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
-      
-@app.route('/portfolio', methods=["GET", "POST"])
+    return render_template("home.html")
+
+
+@app.route("/portfolio", methods=["GET", "POST"])
 def portfolio():
     data = {}
-    if request.method == 'POST':
-        name = request.form['name']
-        quantity = request.form['quantity']
-        price = request.form['price']
-        date = request.form['date']
+    if request.method == "POST":
+        name = request.form["name"]
+        quantity = request.form["quantity"]
+        price = request.form["price"]
+        date = request.form["date"]
 
         if not name:
-            flash('name is required!')
+            flash("name is required!")
         elif not quantity:
-            flash('Quantity is required!')
+            flash("Quantity is required!")
         elif not price:
-            flash('Price is required!')
+            flash("Price is required!")
         else:
-            data['name']=name
-            data['qty']=quantity
-            data['price']=price
-            data['date']=date
-            stocks.append(data)   
-            return redirect(url_for('portfolio'))      
-    return render_template('portfolio.html',stocks=stocks)
+            data["name"] = name
+            data["qty"] = quantity
+            data["price"] = price
+            data["date"] = date
+            stocks.append(data)
+            return redirect(url_for("portfolio"))
+    return render_template("portfolio.html", stocks=stocks)
 
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    return render_template('contact.html')
 
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
